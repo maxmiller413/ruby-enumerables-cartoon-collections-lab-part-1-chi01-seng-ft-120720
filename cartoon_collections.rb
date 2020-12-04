b5e25dbd0ee35a38ab84c7bb2a10a58866514f3c
Max Miller <maxmiller413@gmail.com>
2020-12-04 14:43:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |"element" , index|
+  array.each_with_index do |element , index|
     puts "#{array[element] = index}"
     array
   end 
