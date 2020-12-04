439bc02429dfbfb938c5d0557f5a310e2b55f7b8
Max Miller <maxmiller413@gmail.com>
2020-12-04 14:54:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,10 @@ def greet_characters(array)
   # Print a custom greeting for each element
 end
 
-require 'pry'
 
 def list_dwarves(array)
   array.each_with_index do |element , index|
-    binding.pry 
-    puts "#{array[element]} = index}"
-    array
+    array[element] = index 
   end 
   # Use `each_with_index` to enumerate over the provided array
   #
