85d35150cf229a198e82c58b8a0519df1b3ad7ac
Max Miller <maxmiller413@gmail.com>
2020-12-04 14:54:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |element , index|
-    array[element] = index 
+    array[element] = "index"
   end 
   # Use `each_with_index` to enumerate over the provided array
   #
